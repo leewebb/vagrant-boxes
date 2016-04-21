58da43e77c23838d26a33b6c892418ab30edbfa8
Jon Bartlett <mail@jaybe.net>
2016-04-21 14:24:19 +1000

    add shellcheck

--- generic-dev/bootstrap.sh
+++ generic-dev/bootstrap.sh
@@ -15,8 +15,9 @@ sudo apt-get install -y unzip
 sudo apt-get install -y zip
 sudo apt-get install -y default-jre
 sudo apt-get install -y build-essential g++
+sudo apt-get install -y shellcheck # https://github.com/koalaman/shellcheck
 
-# install system ruby but not the old version that comes as default
+# install system ruby but not the old version as rbenv builds require 2.0+ (not 1.9)
 sudo apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev llvm # for ruby-build
 sudo apt-get install -y software-properties-common
 sudo apt-add-repository -y ppa:brightbox/ruby-ng
