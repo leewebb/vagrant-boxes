20c08e8171c5b74f5d69dbfd6bbd9d6a035533ad
Jon Bartlett <mail@jaybe.net>
2016-06-15 17:39:01 +1000

    change order

--- db2server-dev/x86-32/bootstrap.sh
+++ db2server-dev/x86-32/bootstrap.sh
@@ -53,21 +53,14 @@ sudo su - db2inst1 -c "db2 update dbm cfg using SYSADM_GROUP db2iadm1 ; db2stop 
 sudo su - db2inst1 -c "db2 create db db2unit ; db2 connect to db2unit ; db2 grant dbadm on database to user $USER"
 
 # Install code
-sudo su - db2inst1
-. ~/db2inst1/sqllib/db2profile
-db2 connect to db2unit
 
-# Retrieves and extract log4db2
-cd ; wget https://github.com/angoca/log4db2/releases/download/log4db2-1-Beta-A/log4db2.tar.gz ; tar zxvf log4db2.tar.gz
+# Retrieve, extract and install log4db2
+sudo su - db2inst1 -c "cd ; wget https://github.com/angoca/log4db2/releases/download/log4db2-1-Beta-A/log4db2.tar.gz ; tar zxvf log4db2.tar.gz"
+sudo su - db2inst1 -c "db2 connect to db2unit ; cd ; cd log4db2 ; . ./init ; . ./install"
 
-# Install log4db2
-cd ; cd log4db2 ; . ./install
-
-# Retrieves and extract db2unit
-cd ; wget https://github.com/angoca/db2unit/releases/download/db2unit-1/db2unit.tar.gz ; tar zxvf db2unit.tar.gz
-
-# Install db2unit
-cd ; cd db2unit ; . ./install
+# Retrieve, extract and install db2unit
+sudo su - db2inst1 -c "cd ; wget https://github.com/angoca/db2unit/releases/download/db2unit-1/db2unit.tar.gz ; tar zxvf db2unit.tar.gz"
+sudo su - db2inst1 -c "db2 connect to db2unit ; cd ; cd db2unit ; . ./init ; . ./install"
 
 # Install your code here
 
